3808b0d5b398ac47afe56feae2ac434a3d4824e1
wen2811 <wenbehr@gmail.com>
2023-09-04 23:40:07 +0200

    add components

---
+++ src/components/heroSection/HeroSection.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './HeroSection.css';
+
+const HeroSection = () => {
+    return (
+        <div className="outer-container">
+            <div className="inner-container">
+                <h1>
+                    <strong>A Journey of Self-Care</strong>
+                </h1>
+                <p>
+                    Welcome to Moshi Moshi Spa. The destination where every treatment is a celebration of self, and every moment is an invitation to shine.
+                </p>
+            </div>
+        </div>
+    );
+};
+export default HeroSection;
